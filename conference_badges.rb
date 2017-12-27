ac6f157d4dd2df22726ffe902f9ae60ebfa55ed7
Shivani Mathur <qwellery@gmail.com>
2017-12-27 13:20:39 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,3 +20,12 @@ end
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts "#{badge}"
+  end
+  assign_rooms(attendees).each do |rooms|
+    puts "#{rooms}"
+  end
+end
